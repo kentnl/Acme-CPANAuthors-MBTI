a07bee582b244abc8f43ee6f876c9bfbfc938e7b
Kent Fredric <kentfredric@gmail.com>
2014-12-26 02:44:33 +1300

    Build results of f128370 (on master)

---
+++ lib/Acme/CPANAuthors/MBTI.pm
@@ -0,0 +1,44 @@
+use 5.006;  # our
+use strict;
+use warnings;
+
+package Acme::CPANAuthors::MBTI;
+
+our $VERSION = '0.001000';
+
+# ABSTRACT: Kent Failed To Provide An Abstract
+
+our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
+
+use Moo;
+
+no Moo;
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Acme::CPANAuthors::MBTI - Kent Failed To Provide An Abstract
+
+=head1 VERSION
+
+version 0.001000
+
+=head1 AUTHOR
+
+Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Kent Fredric <kentfredric@gmail.com>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
