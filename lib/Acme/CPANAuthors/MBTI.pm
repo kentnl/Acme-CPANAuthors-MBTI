32ec027c6946f591e30e691abbf6e62cb9d7261a
Kent Fredric <kentfredric@gmail.com>
2014-12-27 00:14:04 +1300

    Initial commit

--- lib/Acme/CPANAuthors/MBTI.pm
+++ lib/Acme/CPANAuthors/MBTI.pm
@@ -1,4 +1,4 @@
-use 5.006;  # our
+use 5.006;    # our
 use strict;
 use warnings;
 
@@ -6,12 +6,42 @@ package Acme::CPANAuthors::MBTI;
 
 our $VERSION = '0.001000';
 
-# ABSTRACT: Kent Failed To Provide An Abstract
+# ABSTRACT: CPAN Authors by MBTI Type
 
 # AUTHORITY
 
-use Moo;
+=head1 DESCRIPTION
 
-no Moo;
+Minimal release atm.
+
+Send bug reports ( or preferably patches ) if you want inclusion.
+
+Will generate extra .pm files on demand as soon as we have members ( Trivial Effort )
+
+Recommended quiz for people who have no idea and just want to play along: L<http://www.25quiz.com/>
+
+Its not guaranteed to be accurate, but that doesn't matter.
+
+Its a generalisation tool.
+
+Just because the generalisation holds that averages of demographic X[0..50] is "Y",
+that does not mean you can take X[1] and assert Y.
+
+However, knowing X[1] is between X[0] and X[50] you can conjecture their odds
+at being relative to the demographic average Y.
+
+Thus, one can conjecture what one I<might> do in a slightly better manner than enumerating
+all possible behaviours observed in humans.
+
+Blah blah blah more ranting here.
+
+Creds to L<< C<ETHER>|https://metacpan.org/author/ETHER >> for the original entry I stole
+all the things from and refactored them to death, L<Acme::CPANAuthors::Nonhuman>
+
+Subsequently, my source file is a lot smaller and needs about 3 variables modifed to make a new type.
+
+So just change the files in C</authorlists>
+
+=cut
 
 1;
---
+++ lib/Acme/CPANAuthors/MBTI/INTP.pm
@@ -0,0 +1,58 @@
+use 5.008;    # utf8
+use strict;
+use warnings;
+use utf8;
+
+package Acme::CPANAuthors::MBTI::INTP;
+
+# ABSTRACT: The TiNe CPAN Authors
+
+# AUTHORITY
+
+our $VERSION = '0.001000';
+#<<<
+{{
+#>>>
+
+    # Template Injector Block
+    require "inc/expand_author_list.pm";
+    our $type     = "INTP";
+    our $category = "MBTI::$type";
+    our $data     = expand_author_list::extract_data( $category, "authorlists/$type.txt" );
+
+    expand_author_list::authors_to_code(
+      {
+        plugin   => $plugin,
+        category => $category,
+        data     => $data,
+      }
+    );
+#<<<
+}}
+#>>>
+
+1;
+
+=pod
+
+=head1 SYNOPSIS
+
+{{
+  # Template Injector Block
+  expand_author_list::generate_synopsis({ 
+    plugin   => $plugin,
+    category => $category,
+    data     => $data,
+  });
+}}
+
+=head1 DESCRIPTION
+
+{{
+  expand_author_list::mbti_description({
+    type     => $type,
+    plugin   => $plugin,
+    category => $category,
+    data     => $data,
+  });
+}}
