d67ac53fee132665c8b54e04333d03527f1a46d8
Kent Fredric <kentfredric@gmail.com>
2014-12-27 11:08:30 +1300

    Build results of 3bf439e (on master)

--- lib/Acme/CPANAuthors/MBTI/INTP.pm
+++ lib/Acme/CPANAuthors/MBTI/INTP.pm
@@ -12,7 +12,7 @@ our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 our $VERSION = '0.001001';
 #<<<
 # Code inserted by inc/expand_author_list#authors_to_code
-# by Dist::Zilla::Plugin::MungeFile::WithDataSection 0.007
+# by Dist::Zilla::Plugin::MungeFile::WithConfigFile 0.002
 ## no critic (ValuesAndExpressions::RestrictLongStrings)
 my %authors  = (
     KENTNL => 'Kent Fredric',
@@ -112,8 +112,8 @@ who have identified themselves as   L<< C<INTP>|https://en.wikipedia.org/wiki/IN
 =for html <div style="text-align:center;padding:0px!important;overflow-y:hidden;
 margin-left: auto; margin-right: auto; max-width: 430px">
 <!-- Data inserted by inc/expand_author_list#authors_to_avatars
- by Dist::Zilla::Plugin::MungeFile::WithDataSection 0.007 -->
-<a href="http://metacpan.org/author/KENTNL"><span><img style="margin: 0 5px 5px 0;" width="80" height="80" src="https://secure.gravatar.com/avatar/4b96677ae9ecbe76b8c33ead244f7407?s=80&d=http%3A%2F%2Fwww.gravatar.com%2Favatar%2F4b96677ae9ecbe76b8c33ead244f7407%3Fs%3D130%26d%3Didenticon" alt="KENTNL" title="KENTNL (Kent Fredric), 149 distributions" /></span></a>
+ by Dist::Zilla::Plugin::MungeFile::WithConfigFile 0.002 -->
+<a href="http://metacpan.org/author/KENTNL"><span><img style="margin: 0 5px 5px 0;" width="80" height="80" src="https://secure.gravatar.com/avatar/4b96677ae9ecbe76b8c33ead244f7407?s=80&d=http%3A%2F%2Fwww.gravatar.com%2Favatar%2F4b96677ae9ecbe76b8c33ead244f7407%3Fs%3D130%26d%3Didenticon" alt="KENTNL" title="KENTNL (Kent Fredric), 150 distributions" /></span></a>
 </div>
 
 For more details see L<< C<Acme::CPANAuthors::MBTI>|Acme::CPANAuthors::MBTI >>.
