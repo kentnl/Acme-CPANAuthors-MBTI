32400cf5536edc91494f73fd4875d48cab7c6663
Kent Fredric <kentfredric@gmail.com>
2014-12-27 00:11:15 +1300

    Build results of f128370 (on master)

--- lib/Acme/CPANAuthors/MBTI.pm
+++ lib/Acme/CPANAuthors/MBTI.pm
@@ -6,13 +6,43 @@ package Acme::CPANAuthors::MBTI;
 
 our $VERSION = '0.001000';
 
-# ABSTRACT: Kent Failed To Provide An Abstract
+# ABSTRACT: CPAN Authors by MBTI Type
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
-use Moo;
 
-no Moo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 1;
 
@@ -24,11 +54,43 @@ __END__
 
 =head1 NAME
 
-Acme::CPANAuthors::MBTI - Kent Failed To Provide An Abstract
+Acme::CPANAuthors::MBTI - CPAN Authors by MBTI Type
 
 =head1 VERSION
 
 version 0.001000
+
+=head1 DESCRIPTION
+
+Minimal release atm.
+
+Send bug reports ( or preferably patches ) if you want inclusion.
+
+Will generate extra .pm files on demand as soon as we have members ( Trivial Effort )
+
+Recommended quiz for people who have no idea and just want to play along: L<http://www.25quiz.com/>
+
+Its not guaranteed to be accurate, but that doesn't matter.
+
+Its a generalisation tool.
+
+Just because the generalisation holds that averages of demographic X[0..50] is "Y",
+that does not mean you can take X[1] and assert Y.
+
+However, knowing X[1] is between X[0] and X[50] you can conjecture their odds
+at being relative to the demographic average Y.
+
+Thus, one can conjecture what one I<might> do in a slightly better manner than enumerating
+all possible behaviours observed in humans.
+
+Blah blah blah more ranting here.
+
+Creds to L<< C<ETHER>|https://metacpan.org/author/ETHER >> for the original entry I stole
+all the things from and refactored them to death, L<Acme::CPANAuthors::Nonhuman>
+
+Subsequently, my source file is a lot smaller and needs about 3 variables modifed to make a new type.
+
+So just change the files in C</authorlists>
 
 =head1 AUTHOR
 
