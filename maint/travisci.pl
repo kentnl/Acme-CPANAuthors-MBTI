41230adaaf0c81935c4e8a213b658201e013c4ef
Kent Fredric <kentfredric@gmail.com>
2015-01-18 23:51:26 +1300

    Build results of e79780c (on master)

--- maint/travisci.pl
+++ maint/travisci.pl
@@ -11,18 +11,14 @@ sub {
   my ($yaml)       = @_;
   my $support      = {};
   my $base_version = version->parse('v5.14');
+  my @extra_allow_fail;
+
   for my $include_matrix ( @{ $yaml->{matrix}->{include} } ) {
     my $support_version = $include_matrix->{perl};
     next unless $support_version;
     next unless version->parse( 'v' . $support_version ) < $base_version;
-    $support->{$support_version} = 1;
+    push @extra_allow_fail, $include_matrix;
   }
-  for my $badversion ( sort keys %{$support} ) {
-    push @{ $yaml->{matrix}->{allow_failures} },
-      {
-      env  => 'TRAVIS_BRANCH=master',
-      perl => $badversion,
-      };
-  }
+  push @{ $yaml->{matrix}->{allow_failures} }, @extra_allow_fail;
   return $yaml;
 };
