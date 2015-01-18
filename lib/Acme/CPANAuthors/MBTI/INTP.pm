f15d95194e3715eda460a722f8a270883be58a3d
Kent Fredric <kentfredric@gmail.com>
2015-01-18 20:40:47 +1300

    Build results of f9818f4 (on master)

--- lib/Acme/CPANAuthors/MBTI/INTP.pm
+++ lib/Acme/CPANAuthors/MBTI/INTP.pm
@@ -113,7 +113,7 @@ who have identified themselves as   L<< C<INTP>|https://en.wikipedia.org/wiki/IN
 margin-left: auto; margin-right: auto; max-width: 430px">
 <!-- Data inserted by inc/expand_author_list#authors_to_avatars
  by Dist::Zilla::Plugin::MungeFile::WithDataSection 0.007 -->
-<a href="http://metacpan.org/author/KENTNL"><span><img style="margin: 0 5px 5px 0;" width="80" height="80" src="https://secure.gravatar.com/avatar/4b96677ae9ecbe76b8c33ead244f7407?s=80&d=http%3A%2F%2Fwww.gravatar.com%2Favatar%2F4b96677ae9ecbe76b8c33ead244f7407%3Fs%3D130%26d%3Didenticon" alt="KENTNL" title="KENTNL (Kent Fredric), 150 distributions" /></span></a>
+<a href="http://metacpan.org/author/KENTNL"><span><img style="margin: 0 5px 5px 0;" width="80" height="80" src="https://secure.gravatar.com/avatar/4b96677ae9ecbe76b8c33ead244f7407?s=80&d=http%3A%2F%2Fwww.gravatar.com%2Favatar%2F4b96677ae9ecbe76b8c33ead244f7407%3Fs%3D130%26d%3Didenticon" alt="KENTNL" title="KENTNL (Kent Fredric), 153 distributions" /></span></a>
 </div>
 
 For more details see L<< C<Acme::CPANAuthors::MBTI>|Acme::CPANAuthors::MBTI >>.
@@ -134,7 +134,7 @@ Kent Fredric <kentnl@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2014 by Kent Fredric <kentfredric@gmail.com>.
+This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
